c6cfb8537b8b55c3af812a16a79d282bab78b0a0
Zryru <jorge.ale91@gmail.com>
2021-10-01 09:34:09 +0200

    testing packery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { Packery, PackeryOptions } from 'packery';
+
+declare var require: any;
+let DraggabillyCttr: any;
+let PackeryCttr: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'ng-packery';
+
+  @ViewChild('grid') public grid!: ElementRef<HTMLDivElement>;
+  @ViewChild('sizer') public sizer!: ElementRef<HTMLDivElement>;
+
+  private pckry!: Packery;
+
+  constructor() {}
+
+  ngOnInit() {
+    PackeryCttr = require('packery');
+    DraggabillyCttr = require('draggabilly');
+  }
+
+  ngAfterViewInit(): void {
+    const options: PackeryOptions = {
+      itemSelector: '.grid-item',
+      columnWidth: '.grid-sizer' as any,
+      gutter: 0,
+
+      percentPosition: true,
+    };
+    this.pckry = new PackeryCttr(this.grid.nativeElement, options);
+
+    console.log(this.pckry.getItemElements());
+
+    this.pckry
+      .getItemElements()
+      .filter((x) => x.classList.contains('grid-item'))
+      .forEach((gridItem, index) => {
+        const draggie = new DraggabillyCttr(gridItem);
+        this.pckry.bindDraggabillyEvents(draggie);
+      });
+  }
 }
